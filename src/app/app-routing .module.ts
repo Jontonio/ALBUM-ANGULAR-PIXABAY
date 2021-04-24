69d384537388b966ff9829bb0aa2468ccfae523d
Jose Antonio <jose.rojascusi@gmail.com>
2021-04-24 13:17:12 +0200

    pre producción

--- src/app/app-routing .module.ts
+++ src/app/app-routing .module.ts
@@ -3,17 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { ImagenComponent } from './pages/imagen/imagen.component';
 import { VectoresComponent } from './pages/vector/vectores.component';
 import { VideoComponent } from './pages/video/video.component';
+import { ImagenesComponent } from './components/imagenes/imagenes.component';
 
 const routes: Routes = [
   { path:"", component:ImagenComponent },
   { path:"imagen", component:ImagenComponent },
   { path:"vector", component:VectoresComponent },
   { path:"video", component:VideoComponent },
+  { path:"**", pathMatch:'full', redirectTo:'' },
 
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{ useHash:true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
